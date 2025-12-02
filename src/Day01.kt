9c2bd857d1775083e647daeeca89e9d0dfbda6b7
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2025-12-02 07:53:19 +0100

    Day1: still ugly, but less so

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,51 +1,37 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-
-        val code = input.toIntList().iterator()
-        var dial = 50
-        var count = 0
-        while (code.hasNext()) {
-            dial = (dial + code.next()) % 100
-            if (dial == 0) count++
-        }
-        return count
+        return countZeros(code = input.toIntList().iterator())
     }
 
-    fun part2(input: List<String>): Long {
-        val code = input.toIntList().iterator()
-        var dial = 50
-        var count = 0L
-        while (code.hasNext()) {
-            val n = code.next()
-            if (n > 0) {
-                (1..n).forEach {
-                    dial = (dial + 1) % 100
-                    if (dial == 0)
-                        count++
-                }
-            } else {
-                (n..-1).forEach {
-                    dial = (dial - 1) % 100
-                    if (dial == 0)
-                        count++
-                }
-            }
-        }
-        return count
+    fun part2(input: List<String>): Int {
+        return countZeros(code = input.toIntList().iterator(), countIntermediate = true)
     }
-    // Or read a large test input from the `src/Day01_test.txt` file:
+
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 3)
-    check(part2(testInput) == 6L)
+    check(part2(testInput) == 6)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     println("Part 1:")
     part1(input).writeToConsole()
     println("Part 2:")
     part2(input).writeToConsole()
+}
 
-
+private fun countZeros(code: Iterator<Int>, countIntermediate: Boolean = false): Int {
+    var count = 0
+    var dial = 50
+    while (code.hasNext()) {
+        val n = code.next()
+        repeat(abs(n)) {
+            dial = (dial + n / abs(n)) % 100
+            if (dial == 0 && countIntermediate) count++
+        }
+        if (dial == 0 && !countIntermediate) count++
+    }
+    return count
 }
 
 fun List<String>.toIntList(): List<Int> {
