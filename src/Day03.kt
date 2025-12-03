802370a82915abf9ff03943636dfee6916ce73bc
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2025-12-03 08:46:09 +0100

    Day3: recursive :-)

---
+++ src/Day03.kt
@@ -0,0 +1,33 @@
+import kotlin.collections.dropLast
+
+fun main() {
+    fun part1(input: List<String>): Long {
+        return input.sumOf {
+            it.map { c -> c.digitToInt().toLong() }.recursiveJoltage(2).toLong()
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        return input.sumOf {
+            it.map { c -> c.digitToInt().toLong() }.recursiveJoltage(12).toLong()
+        }
+    }
+
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 357L)
+    check(part2(testInput) == 3121910778619L)
+
+    val input = readInput("Day03")
+    println("Part 1:")
+    part1(input).writeToConsole()
+    println("Part 2:")
+    part2(input).writeToConsole()
+}
+
+fun List<Long>.recursiveJoltage(num: Int): String {
+    if (num == 1) return max().toString()
+    else {
+        val max = dropLast(num - 1).max()
+        return max.toString() + drop(indexOf(max) +1).recursiveJoltage(num -1)
+    }
+}
